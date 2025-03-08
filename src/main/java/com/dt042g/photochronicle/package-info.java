8e7569d9074ac24bac338e8c771b475c33435645
jola2311 <jola2311@student.miun.se>
2025-03-08 15:02:08 +0200

    Add package-info files
    Also update the metadata extractor dependency to the latest version and add a git ignore for personal-notes.md.

---
+++ src/main/java/com/dt042g/photochronicle/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Entry-point for the application.
+ *
+ * <p>This package should only contain Main.java and its main method should just instantiate the
+ * PhotoChronicle controller which will handle the rest.</p>
+ */
+package com.dt042g.photochronicle;
